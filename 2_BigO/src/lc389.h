10fca3a6abe9f4bc55cc307673a41e306278ebe6
unta1337 <firstapple42@gmail.com>
2021-09-10 03:12:54 +0900

    LeetCode 389, 771 주석 추가

--- 2_BigO/src/lc389.h
+++ 2_BigO/src/lc389.h
@@ -4,6 +4,7 @@
  */
 // https://leetcode.com/problems/find-the-difference/
 
+// Status: Accepted
 // Runtime: 3 ms (77.59 %)
 // Memory Usage: 6.7 MB (66.79 %)
 // https://leetcode.com/submissions/detail/551563014/
@@ -13,22 +14,35 @@
 
 namespace Solution389 {
     char findTheDifference(std::string s, std::string t) {
+        // 소문자 알파벳이 등장한 횟수를 기록할 두 개의 배열 선언.
         short s_char[26] = { 0, };
         short t_char[26] = { 0, };
         
+        // s와 t중 s의 길이만큼 반복하여 NullPointerExceptio을 방지.
         for (int i = 0; i < s.length(); i++) {
             s_char[s[i] - 'a']++;
             t_char[t[i] - 'a']++;
-        } t_char[t[t.length() - 1] - 'a']++;
+        } t_char[t[t.length() - 1] - 'a']++;    // t의 마지막 원소에 대한 처리를 수행.
 
+        // 알파벳 등장 횟수를 비교하여 횟수가 다른 알파벳을 반환.
         for (int i = 0; i < 26; i++) {
             if (s_char[i] != t_char[i])
                 return i + 'a';
         }
         
+        // 함수 반환형 오류 방지.
         return '\0';
     }
 };
+
+/*
+ * 입력되는 문자열 s의 길이를 n이라 두자.
+ *
+ * 최초 s, t 탐색 시 O(n) + O(1) -> O(n)
+ * 알바벳 등장 횟수 비교 시 O(26) -> O(1)
+ * 
+ * 따라서, 해당 알고리즘의 빅 오는 O(n) + O(1) -> O(n)
+ */
 
 using namespace Solution389;
 
--- 2_BigO/src/lc771.h
+++ 2_BigO/src/lc771.h
@@ -4,6 +4,7 @@
  */
 // https://leetcode.com/problems/jewels-and-stones/
 
+// Status: Accepted
 // Runtime: 0 ms (100.00 %)
 // Memory Useage: 6.1 MB (96.70 %)
 // https://leetcode.com/submissions/detail/551589720/
@@ -13,10 +14,12 @@
 
 namespace Solution771 {
     int numJewelsInStones(std::string jewels, std::string stones) {
+        // 알바벳 대문자와 소문자의 등장 횟수를 기록할 배열 선언 및 등장 횟수 계산.
         bool j[58] = { 0, };
         for (int i = 0; i < jewels.length(); i++)
             j[jewels[i] - 'A'] = true;
         
+        // 입력으로 들어온 stone을 탐색하면서 해당 알파벳이 jewels에 있으면 카운트 증가.
         int count = 0;
         for (int i = 0; i < stones.length(); i++)
             count += j[stones[i] - 'A'];
@@ -24,6 +27,16 @@ namespace Solution771 {
         return count;
     }
 };
+
+/*
+ * jewels의 길이를 n, stone의 길이를 m이라 두자.
+ *
+ * 최초 jewels 탐색 시 O(n)
+ * 이후 알바벳 유무 탐색 시 O(m)
+ * 
+ * 따라서, 해당 알고리즘의 빅 오는 O(n) + O(m) -> O(n + m)
+ * 또는, 입력되는 문자열의 총 길이를 n으로 하여: O(n)
+ */
 
 using namespace Solution771;
 
