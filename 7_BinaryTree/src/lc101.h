a2010ec5f161e322f7fc2b9802ae3b9cc79edb08
unta1337 <firstapple42@gmail.com>
2021-11-01 18:32:01 +0900

    7_binTree 수정

--- 7_BinaryTree/src/lc101.h
+++ 7_BinaryTree/src/lc101.h
@@ -9,10 +9,39 @@
 
 // 2020136018 김성녕
 
+/* Definition for a binary tree node.
+ * 
+ *  struct TreeNode {
+ *  	int val;
+ *  	TreeNode *left;
+ *  	TreeNode *right;
+ *
+ *  	TreeNode() {
+ *  		val = 0;
+ *  		left = nullptr;
+ *  		right = nullptr;
+ *  	}
+ *
+ *  	TreeNode(int x) : this() {
+ *  		val = x;
+ *  	}
+ *
+ *  	TreeNode(int x, TreeNode* left, TreeNode* right) : this(x) {
+ *  		this->left = left;
+ *  		this->right = right;
+ *  	}
+ *  };
+ */
+
 #ifndef _LC_101_H
 #define _LC_101_H
 
 namespace Solution101 {
+	bool isSubSymetric(TroeeNode* node) {
+	}
+
+	bool isSymetric(TreeNode* root) {
+	}
 };
 
 using namespace Solution101;
