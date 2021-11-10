947d6884297accf302410677dc64cdfbeee6dd68
S.Y. Kim <firstapple42@gmail.com>
2021-11-10 18:23:31 +0900

    8. 이진 탐색 트리: 수정된 코드 추가

--- 8_BinarySearchTree/src/AllTests.cpp
+++ 8_BinarySearchTree/src/AllTests.cpp
@@ -1,7 +1,7 @@
 /*
  * @copyright 한국기술교육대학교 컴퓨터공학부 자료구조및실습
  * @version 2021년도 2학기
- * @author refracta
+ * @author 김상진
  * @file AllTests.cpp
  * 테스트 프로그램을 실행하기 위한 main 함수
  */
--- 8_BinarySearchTree/src/BST.h
+++ 8_BinarySearchTree/src/BST.h
@@ -1,9 +1,9 @@
 /*
  * @copyright 한국기술교육대학교 컴퓨터공학부 자료구조및실습
  * @version 2021년도 2학기
- * @author refracta
+ * @author 김상진
  * @file BST.h
- * 이진 검색 트리: 부모에 대한 포인터를 유지하지 않음, 재귀적으로 구현
+ * 이진 검색 트리: 부모에 대한 포인터를 유지, 재귀적으로 구현
  * 코드 중복 제거
  */
 
@@ -17,6 +17,7 @@
 #include <vector>
 #include <iostream>
 #include <queue>
+#include <stack>
 
 class BST {
 public:
@@ -26,19 +27,50 @@ public:
 private:
     struct TreeNode {
         int key;
+        TreeNode *parent;
         TreeNode *left;
         TreeNode *right;
-        TreeNode *parent;
 
-        TreeNode(int key = -1, TreeNode *left = nullptr, TreeNode *right = nullptr, TreeNode *parent = nullptr) :
-                key{key}, left{left}, right{right}, parent{parent} {}
+        TreeNode(int key = -1, TreeNode *parent = nullptr, TreeNode *left = nullptr, TreeNode *right = nullptr) :
+                parent{parent}, key{key}, left{left}, right{right} {}
+    };
+
+    template<typename U>
+    class TreeIterator : public std::iterator<std::input_iterator_tag, U> {
+        typename std::vector<U> visitedOrder;
+        U *p{nullptr};
+    public:
+        explicit TreeIterator(const BST *bst) {
+            if (bst) {
+                bst->traversal(visitedOrder);
+                p = &visitedOrder[0];
+            }
+        }
+
+        const TreeIterator &operator++() noexcept {
+            ++p;
+            if (p == &visitedOrder[visitedOrder.size()]) { p = nullptr; }
+            return *this;
+        }
+
+        TreeIterator operator++(int) noexcept {
+            auto retval{*this};
+            ++p;
+            if (p == &visitedOrder[visitedOrder.size()]) { p = nullptr; }
+            return retval;
+        }
+
+        bool operator==(const TreeIterator &other) const noexcept { return p == other.p; }
+
+        bool operator!=(const TreeIterator &other) const noexcept { return p != other.p; }
+
+        U &operator*() noexcept { return *p; }
     };
 
     TreeNode *root{nullptr};
     size_t numNodes{0};
-
-    std::vector<int> visitedOrder;
     TreeTraversal traversalMethod{TreeTraversal::INORDER};
+
 public:
     BST() = default;
 
@@ -51,7 +83,8 @@ public:
     }
 
     BST(const BST &other) {
-        preorder(other.root);
+        std::vector<int> visitedOrder;
+        preorder(other.root, visitedOrder);
         for (auto key: visitedOrder) { add(key); }
     }
 
@@ -64,8 +97,8 @@ public:
 
     const BST &operator=(const BST &other) {
         clear();
-        visitedOrder.clear();
-        preorder(other.root);
+        std::vector<int> visitedOrder;
+        preorder(other.root, visitedOrder);
         for (auto key: visitedOrder) { add(key); }
         return *this;
     }
@@ -102,23 +135,16 @@ public:
         TreeNode *newNode{new TreeNode{key}};
         if (isEmpty()) {
             root = newNode;
-            ++numNodes;
         } else {
             TreeNode *parent{findNode(root, key)};
             if (parent->key == key) {
                 delete newNode;
                 return;
-            } else if (parent->key > key) {
-                parent->left = newNode;
-                newNode->parent = parent;
-                // 새로 생성한 노드에 부모 노드 할당
-            } else {
-                parent->right = newNode;
-                newNode->parent = parent;
-                // 새로 생성한 노드에 부모 노드 할당
-            }
-            ++numNodes;
+            } else if (parent->key > key) { parent->left = newNode; }
+            else { parent->right = newNode; }
+            newNode->parent = parent;
         }
+        ++numNodes;
     }
 
     bool find(int key) const noexcept {
@@ -129,7 +155,6 @@ public:
     int next(int key) const {
         if (isEmpty()) { throw std::runtime_error("빈 트리"); }
         TreeNode *currNode{findNode(root, key)};
-        // parents stack을 이용하지 않도록 변경
         if (currNode->key != key) { throw std::invalid_argument("키가 존재하지 않음"); }
         return getNext(currNode);
     }
@@ -137,30 +162,24 @@ public:
     int prev(int key) const {
         if (isEmpty()) { throw std::runtime_error("빈 트리"); }
         TreeNode *currNode{findNode(root, key)};
-        // parents stack을 이용하지 않도록 변경
         if (currNode->key != key) { throw std::invalid_argument("키가 존재하지 않음"); }
         return getPrev(currNode);
     }
 
     void remove(int key) noexcept {
         if (isEmpty()) { return; }
-        // parents stack을 이용하지 않도록 변경
         TreeNode *delNode{findNode(root, key)};
         if (delNode->key != key) { return; }
 
         if (delNode->left && delNode->right) {
-            TreeNode *parent{delNode};
             TreeNode *prevNode{delNode->left};
             while (prevNode->right) {
-                parent = prevNode;
                 prevNode = prevNode->right;
             }
             delNode->key = prevNode->key;
-            removeChild(parent, prevNode);
-        } else {
-            TreeNode *parent{!delNode->parent ? nullptr : delNode->parent};
-            removeChild(parent, delNode);
+            delNode = prevNode;
         }
+        removeChild(delNode);
     }
 
     std::vector<int> rangeSearch(int left, int right) const {
@@ -181,17 +200,13 @@ public:
         } else {
             bool prevFound{false};
             bool nextFound{false};
-            TreeNode dummy = {-1, nullptr, nullptr, currNode};
-            currNode = &dummy;
-            // 더미 노드를 통한 순회 처리
-            while (currNode->parent && !(nextFound && prevFound)) {
-                TreeNode *parent{currNode->parent};
-                if (!nextFound && parent->key > key) {
-                    result.second = parent->key;
+            while (currNode && !(nextFound && prevFound)) {
+                if (!nextFound && currNode->key > key) {
+                    result.second = currNode->key;
                     nextFound = true;
                 }
-                if (!prevFound && parent->key < key) {
-                    result.first = parent->key;
+                if (!prevFound && currNode->key < key) {
+                    result.first = currNode->key;
                     prevFound = true;
                 }
                 currNode = currNode->parent;
@@ -203,24 +218,19 @@ public:
     }
 
     void balanceTree() {
-        visitedOrder.clear();
-        inorder(root);
+        std::vector<int> visitedOrder;
+        inorder(root, visitedOrder);
         clear();
-        balanceTree(0, visitedOrder.size() - 1);
+        balanceTree(0, visitedOrder.size() - 1, visitedOrder);
     }
 
     void setIteratorType(BST::TreeTraversal traversalMethod) noexcept {
         this->traversalMethod = traversalMethod;
     }
 
-    std::vector<int>::iterator begin() {
-        traversal();
-        return visitedOrder.begin();
-    }
+    auto begin() const { return TreeIterator<int>(this); }
 
-    std::vector<int>::iterator end() {
-        return visitedOrder.end();
-    }
+    auto end() const { return TreeIterator<int>(nullptr); }
 
 private:
     size_t size(TreeNode *currNode) const noexcept {
@@ -229,20 +239,6 @@ private:
 
     long height(TreeNode *currNode) const noexcept {
         return currNode ? 1 + std::max(height(currNode->left), height(currNode->right)) : 0L;
-    }
-
-    void traversal() {
-        visitedOrder.clear();
-        switch (traversalMethod) {
-            case TreeTraversal::INORDER:
-                inorder(root);
-                break;
-            case TreeTraversal::PREORDER:
-                preorder(root);
-                break;
-            default:
-                postorder(root);
-        }
     }
 
     void deleteNodes(TreeNode *currNode) {
@@ -261,54 +257,47 @@ private:
         }
     }
 
-    void removeChild(TreeNode *parent, TreeNode *child) {
-        // parents stack을 이용하지 않도록 변경
-        TreeNode *descendant{child->left ? child->left : child->right};
-        if (!parent) { root = descendant; }
+    void removeChild(TreeNode *node) {
+        TreeNode *parent{node->parent};
+        TreeNode *child{node->left ? node->left : node->right};
+        if (!parent) { root = child; }
         else {
-            if (parent->key > child->key) { parent->left = descendant; }
-            else { parent->right = descendant; }
+            if (parent->key >= node->key) { parent->left = child; }
+            else { parent->right = child; }
         }
-        delete child;
+        if (child) { child->parent = parent; }
+        delete node;
         --numNodes;
     }
 
     int getNext(TreeNode *currNode) const {
-        // parents stack을 이용하지 않도록 변경
-        TreeNode *originalNode = currNode;
         if (currNode->right) {
             currNode = currNode->right;
             while (currNode->left) { currNode = currNode->left; }
             return currNode->key;
         } else {
+            int key = currNode->key;
             while (currNode->parent) {
-                TreeNode *parent = currNode->parent;
-                if (parent->key > currNode->key) {
-                    return parent->key;
-                }
                 currNode = currNode->parent;
+                if (currNode->key > key) { return currNode->key; }
             }
+            return key; // 주어진 키가 BST에서 가장 큰 값 (next가 없는 경우)
         }
-        return originalNode->key;
     }
 
     int getPrev(TreeNode *currNode) const {
-        // parents stack을 이용하지 않도록 변경
-        TreeNode *originalNode = currNode;
         if (currNode->left) {
             currNode = currNode->left;
             while (currNode->right) { currNode = currNode->right; }
             return currNode->key;
         } else {
+            int key = currNode->key;
             while (currNode->parent) {
-                TreeNode *parent = currNode->parent;
-                if (parent->key < currNode->key) {
-                    return parent->key;
-                }
                 currNode = currNode->parent;
+                if (currNode->key < key) { return currNode->key; }
             }
+            return key; // 주어진 키가 BST에서 가장 작은 값 (prev가 없는 경우)
         }
-        return originalNode->key;
     }
 
 
@@ -318,7 +307,7 @@ private:
         if (curr->right && curr->right->key <= right) { inorderRangeSearch(left, right, curr->right, result); }
     }
 
-    void balanceTree(size_t lo, size_t hi) {
+    void balanceTree(size_t lo, size_t hi, const std::vector<int> &visitedOrder) {
         if (lo == hi) { add(visitedOrder[lo]); }
         else if (lo + 1 == hi) {
             add(visitedOrder[hi]);
@@ -326,26 +315,39 @@ private:
         } else {
             size_t mid{lo + (hi - lo) / 2};
             add(visitedOrder[mid]);
-            balanceTree(lo, mid - 1);
-            balanceTree(mid + 1, hi);
+            balanceTree(lo, mid - 1, visitedOrder);
+            balanceTree(mid + 1, hi, visitedOrder);
         }
     }
 
-    void preorder(TreeNode *curr) {
-        visitedOrder.push_back(curr->key);
-        if (curr->left) { preorder(curr->left); }
-        if (curr->right) { preorder(curr->right); }
+    void traversal(std::vector<int> &visitedOrder) const {
+        switch (traversalMethod) {
+            case TreeTraversal::INORDER:
+                inorder(root, visitedOrder);
+                break;
+            case TreeTraversal::PREORDER:
+                preorder(root, visitedOrder);
+                break;
+            default:
+                postorder(root, visitedOrder);
+        }
     }
 
-    void inorder(TreeNode *curr) {
-        if (curr->left) { inorder(curr->left); }
+    void preorder(TreeNode *curr, std::vector<int> &visitedOrder) const {
         visitedOrder.push_back(curr->key);
-        if (curr->right) { inorder(curr->right); }
+        if (curr->left) { preorder(curr->left, visitedOrder); }
+        if (curr->right) { preorder(curr->right, visitedOrder); }
     }
 
-    void postorder(TreeNode *curr) {
-        if (curr->left) { postorder(curr->left); }
-        if (curr->right) { postorder(curr->right); }
+    void inorder(TreeNode *curr, std::vector<int> &visitedOrder) const {
+        if (curr->left) { inorder(curr->left, visitedOrder); }
+        visitedOrder.push_back(curr->key);
+        if (curr->right) { inorder(curr->right, visitedOrder); }
+    }
+
+    void postorder(TreeNode *curr, std::vector<int> &visitedOrder) const {
+        if (curr->left) { postorder(curr->left, visitedOrder); }
+        if (curr->right) { postorder(curr->right, visitedOrder); }
         visitedOrder.push_back(curr->key);
     }
 };
